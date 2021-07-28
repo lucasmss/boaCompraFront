54917c908c4b32bbe56606edca4c185503303029
lucasmss <lucasmss16@gmail.com>
2021-07-27 22:02:20 -0300

    Ajuste de front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBDN7OcPp1fUlql6RDpm44sxmVb4kcb3CQ",
+    authDomain: "boacompraapi.firebaseapp.com",
+    projectId: "boacompraapi",
+    storageBucket: "boacompraapi.appspot.com",
+    messagingSenderId: "177743908311",
+    appId: "1:177743908311:web:679a5afc22f308d7f84f92",
+    measurementId: "G-GBXZ51P4SD"
+  },
 };
